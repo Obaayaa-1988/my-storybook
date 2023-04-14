38b81f7dd81778ee1dd91789b71a1420134e1697
Abigail <ashbella333@gmail.com>
2023-04-14 17:13:46 +0000

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Card from './components/Card.vue';
+import Button from './components/Button.vue';
+</script>
+<template>
+    <Card title="Hello"></Card>
+    <Button label="Hello"></Button>
+</template>
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
