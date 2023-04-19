2bd42d0adca2d32bde0ac4a229168e61ec57052a
Abigail <ashbella333@gmail.com>
2023-04-19 16:05:45 +0000

    added logic for component stories and templates

--- src/components/PaButton.stories.js
+++ src/components/PaButton.stories.js
@@ -1,23 +1,21 @@
 import PaButton from './PaButton.vue';
 
 export default {
-    title: 'PaButton',
-    component: PaButton,
-    tags:['autodocs'],
-    argsTypes: {
-        backgroundColr: {
-            control: 'color',
-        },
-
-        onClick: {},
-        size: {
-            control: {
-                type: 'select',
-            },
-            options: ['small', 'medium', 'large'],
-
-        },
+  title: "PaButton",
+  component: PaButton,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: {
+      control: "color",
     },
+    onClick: {},
+    size: {
+      control: {
+        type: "select",
+      },
+      options: ["small", "medium", "large"],
+    },
+  },
 };
 
 export const Primary = {
@@ -25,4 +23,32 @@ export const Primary = {
     primary: true,
     title: "Submit",
   },
+
+  
 };
+
+export const Small = {
+    args:{
+        small: true,
+        title: "send"
+
+    }
+
+}; 
+
+export const Medium = {
+    args: {
+        medium: true,
+        title: "click me"
+
+    }
+
+};
+
+export const Large = {
+    args:{
+        large: true,
+        title: "forgotten password"
+
+    }
+}
