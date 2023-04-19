49369a24e39399db4ac233b9df727d54a9f403b0
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-19 21:52:53 +0100

    created new component for header

---
+++ src/components/PaHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=" rounded-md w-96 p-4 shadow-inner" :class="backgroundColor">
+        <h4 class="text-4xl text-white">{{ title }}</h4>
+        <p class="secondary text-white mt-4">{{ subText }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaHeader',
+    props: {
+        title: String,
+        subText: String,
+        backgroundColor: String
+    }
+
+}
+</script>
+
+<style scoped></style>
