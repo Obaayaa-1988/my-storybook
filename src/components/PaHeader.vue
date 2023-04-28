3e3ca0a0a33224587435b03b671ed0ce249b54e8
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-28 16:36:57 +0100

    added font bold to nav links

--- src/components/PaHeader.vue
+++ src/components/PaHeader.vue
@@ -2,11 +2,17 @@
     <div class=" rounded-md w-[90%] p-4 shadow-inner" :class="backgroundColor">
         <nav class="flex justify-between">
             <div class="text-white font-mono text-2xl py-2 ">Logo</div>
-            <ul class="text-white font-mono flex justify-between">
-                <li class="pr-8">Home</li>
+            <ul class="text-white  font-semibold py-2 flex justify-between">
+                <li class="pr-8  ">Home</li>
                 <li class="pr-8">About</li>
                 <li class="pr-8">Contact</li>
             </ul>
+            <div> 
+                <div></div>
+                <div></div>
+                <div></div>
+
+            </div>
         </nav>
     </div>
 </template>
