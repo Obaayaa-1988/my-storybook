b5d69be052723d6e15b10a1a13c854c2ff54d663
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-29 22:00:20 +0100

    added header instances to export default fumction

--- src/components/PaHeader.stories.js
+++ src/components/PaHeader.stories.js
@@ -17,18 +17,25 @@ const Template = (args) => ({
 export const FirstStory = Template.bind({});
 
 FirstStory.args = {
-  title: "Primary",
-  subText: "Example sub text",
+  link: "Home",
+  linkOne: "About",
+  linkTwo: "Contact",
   backgroundColor: "bg-black",
 };
 export const SecondStory = Template.bind({});
 
 SecondStory.args = {
+  link: "Home",
+  linkOne: "About",
+  linkTwo: "Contact",
   backgroundColor: "bg-pink-400",
 };
 
 export const ThirdStory = Template.bind({});
 
 ThirdStory.args = {
+  link: "Home",
+  linkOne: "About",
+  linkTwo: "Contact",
   backgroundColor: "bg-blue-600",
 };
