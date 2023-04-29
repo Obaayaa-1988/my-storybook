65205c138de70c574ccd597e255c967359dd0734
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-29 21:59:41 +0100

    corrected props error

--- src/components/PaHeader.vue
+++ src/components/PaHeader.vue
@@ -5,7 +5,7 @@
             <ul class="text-white  font-semibold py-4 flex justify-between text-[18px] cursor-pointer">
                 <li class="pr-8  ">{{ link }}</li>
                 <li class="pr-8">{{ linkOne   }}</li>
-                <li class="pr-8">{{ linkTWo }}</li>
+                <li class="pr-8">{{ linkTwo }}</li>
             </ul>
             <div class="flex justify-between my-4 cursor-pointer"> 
                 <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6 mr-4 fill-white"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/></svg></div>
@@ -23,7 +23,7 @@ export default {
     props: {
         link: String, 
         linkOne: String, 
-        linkTWo: String, 
+        linkTwo: String, 
         backgroundColor: String
     }
 
